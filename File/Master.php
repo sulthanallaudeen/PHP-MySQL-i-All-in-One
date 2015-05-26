ac7f2c12c4b10fa3f20e531a368441e7817c9d48
Sulthan Allaudeen <sulthanallaudeen@users.noreply.github.com>
2015-05-26 16:26:07 +0530

    Update Master.php
    Updated with Foreach Iteration

--- File/Master.php
+++ File/Master.php
@@ -25,6 +25,31 @@ $Connection->query("CREATE TABLE IF NOT EXISTS `users` (
 
 $Connection->query("INSERT INTO users VALUES (1, 'sulthan', 'password', 'sa@sysaxiom.com', 'Chennai')");
 
+#Executing Multiple Insert Query
+
+$Connection->query("INSERT INTO users VALUES (2, 'sulthan', 'password', 'sulthan@sysaxiom.com', 'Delhi'), 
+											 (3, 'helen', 'keller', 'helen@sysaxiom.com', 'Mumbai')");
+
+#Selected limited set of records
+
+$Result = $Connection->query("SELECT * FROM users ORDER by id ASC LIMIT 1,2");
+
+#If Result Exists
+
+if ($Result) 
+{
+	#Start Iteration through $Result
+
+    while ($Data = $Result->fetch_assoc()) 
+    {
+    	echo $Data["id"].' - '.$Data["username"].'<br>';
+    }
+
+    #And Free the $Result
+
+    $Result->free();
+}
+
 #Executing Update Query
 
 $Connection->query("UPDATE users SET city = 'Chennai, India' WHERE id = 1");
@@ -35,7 +60,7 @@ $Count = $Connection->query("SELECT * FROM users")->num_rows;
 
 #Executing Delete Statement
 
-$Connection->query("DELETE FROM users WHERE id = 7");
+$Connection->query("DELETE FROM users WHERE id = 1");
 
 #Executing the Delete Statement for one condition
 
